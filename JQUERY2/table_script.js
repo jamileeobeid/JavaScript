1b4e28e1732ae223a154048828016a2eaf2225cb
Jamilé Obeid <jamileemhd@gmail.com>
2025-01-31 19:48:26 +0400

    Update table_script.js

--- JQUERY2/table_script.js
+++ JQUERY2/table_script.js
@@ -10,7 +10,6 @@ function showData(data){
 }
 
 function generateTable(table, data){
-
     for(let element of data){
         let farenheit = (9/5 * element.temperature) + 32;
         let weather = `<tr><td>${element.city}</td><td>${element.temperature}</td><td>${farenheit}</td></tr>`
